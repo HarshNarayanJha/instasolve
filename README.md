75a75b767c82b2c0e782f4487559045ede7b979e
Harsh Narayan Jha <50262541+HarshNarayanJha@users.noreply.github.com>
2024-07-30 17:54:50 +0530

    initial commit

---
+++ README.md
@@ -0,0 +1,21 @@
+# InstaSolve Landing Page
+
+Landing Page for my App Insta Solve made in Vue.js and Vite!
+
+## Project Setup
+
+```sh
+bun install
+```
+
+### Compile and Hot-Reload for Development
+
+```sh
+bun dev
+```
+
+### Compile and Minify for Production
+
+```sh
+bun build
+```
---
+++ src/App.vue
@@ -0,0 +1,21 @@
+<script setup></script>
+
+<template>
+    <header></header>
+
+    <main></main>
+</template>
+
+<style scoped>
+header {
+    line-height: 1.5;
+}
+
+@media (min-width: 1024px) {
+    header {
+        display: flex;
+        place-items: center;
+        padding-right: calc(var(--section-gap) / 2);
+    }
+}
+</style>
