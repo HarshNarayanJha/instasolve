c338d2a0e70071d9a68fa62af2aad8e5012d6eef
Harsh Narayan Jha <50262541+HarshNarayanJha@users.noreply.github.com>
2024-08-10 22:48:39 +0530

    fix: windows weird scaling bug fix via zoom on html

--- src/main.js
+++ src/main.js
@@ -1,6 +1,9 @@
-import './assets/main.css'
+import "./assets/main.css"
 
-import { createApp } from 'vue'
-import App from './App.vue'
+import { createApp } from "vue"
+import App from "./App.vue"
 
-createApp(App).mount('#app')
+createApp(App).mount("#app")
+
+const factor = window.devicePixelRatio == 1 ? 1 : 1.6 / window.devicePixelRatio
+document.querySelector("html").style.zoom = factor
